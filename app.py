bb9462a54317e5bd56fe609afde343e133d1299d
jwongUlti25 <jwong25@stuy.edu>
2019-01-08 22:16:13 -0500

    added authentication to app.py with database usage

--- app.py
+++ app.py
@@ -1,5 +1,30 @@
-from flask import Flask, render_template
+# LetNogo - Joan Chirinos, Susan Lin, Johnny Wong, Thomas Zhao
+# SoftDev1 pd8
+# P02: The End
+
+import os
+
+from flask import Flask, render_template, redirect, url_for, session, request, flash, get_flashed_messages
+
+from util import database
+
+#============instantiate Flask object================
 app = Flask(__name__)
+app.secret_key = os.urandom(32)
+
+#===========manage user data here====================
+
+DB_FILE = 'data/tuesday.db'
+user = None
+
+def setUser(userName):
+    '''
+    Sets the user global variable to userName
+    '''
+    global user
+    user = userName
+
+
 
 @app.route('/')
 def index():
@@ -8,6 +33,51 @@ def index():
 @app.route('/home')
 def home():
     return render_template('landing.html')
+
+@app.route('/authenticate', methods=['POST'])
+def authenticate():
+    '''
+    References DB_FILE and handles authentication and registration
+    '''
+    if user in session:
+        return redirect(url_for('index'))
+    data = database.DB_Manager(DB_FILE)
+    username, password = request.form['username'], request.form['password']
+
+    #=====LOG IN=====
+    print(request.form['submit'])
+    if request.form['submit'] == 'Login':
+        # username and password are valid
+        if len(username.strip()) != 0 and len(password.strip()) != 0 and data.verifyUser(username, password):
+            session[username] = password
+            setUser(username)
+            data.save()
+            flash('Successfully logged in!')
+            return redirect(url_for('home'))
+        # user was found but password is incorrect
+        elif data.findUser(username):
+            flash('Incorrect password!')
+        # user not found in DB at all
+        else:
+            flash('Incorrect username!')
+        data.save()
+
+    #=====REGISTER=====
+    else:
+        if len(username.strip()) != 0 and not data.findUser(username):
+            if len(password.strip()) != 0:
+                # add account to DB
+                data.registerUser(username, password)
+                data.save()
+                flash("Successfully registered account for user '{0}'".format(username))
+            else:
+                flash('Password length insufficient!')
+        elif len(username.strip()) == 0:
+            flash('Username length insufficient!')
+        else:
+            flash('Username already exists!')
+    # try again
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.debug = True
---
+++ util/database.py
@@ -0,0 +1,164 @@
+# LetNogo - Joan Chirinos, Susan Lin, Johnny Wong, Thomas Zhao
+# SoftDev1 pd8
+# P02: The End
+
+import sqlite3   # enable control of an sqlite database
+
+class DB_Manager:
+    '''
+    HOW TO USE:
+    Every method openDB by connecting to the inputted path of
+    a database file. After performing all operations on the
+    database, the instance of the DB_Manager must save using
+    the save method.
+    The operations/methods can be found below. DB_Manager
+    has been custom fit to work with
+    P02: The End
+    '''
+    def __init__(self, dbfile):
+        '''
+        SET UP TO READ/WRITE TO DB FILES
+        '''
+        self.DB_FILE = dbfile
+        self.db = None
+    #========================HELPER FXNS=======================
+    def openDB(self):
+        '''
+        OPENS DB_FILE AND RETURNS A CURSOR FOR IT
+        '''
+        self.db = sqlite3.connect(self.DB_FILE) # open if file exists, otherwise create
+        return self.db.cursor()
+
+    def tableCreator(self, tableName, col0, col1, col2):
+        '''
+        CREATES A 3 COLUMN TABLE IF tableName NOT TAKEN
+        ALL PARAMS ARE STRINGS
+        '''
+        c = self.openDB()
+        if not self.isInDB(tableName):
+            command = 'CREATE TABLE "{0}"({1}, {2}, {3});'.format(tableName, col0, col1, col2)
+            c.execute(command)
+
+
+    def insertRow(self, tableName, data):
+       '''
+         APPENDS data INTO THE TABLE THAT CORRESPONDS WITH tableName
+         @tableName is the name the table being written to
+         @data is a tuple containing data to be entered
+       '''
+       c = self.openDB()
+       command = 'INSERT INTO "{0}" VALUES(?, ?, ?)'
+       c.execute(command.format(tableName), data)
+
+
+    def isInDB(self, tableName):
+        '''
+        RETURNS True IF THE tableName IS IN THE DATABASE
+        RETURNS False OTHERWISE
+        '''
+        c = self.openDB()
+        command = 'SELECT * FROM sqlite_master WHERE type = "table"'
+        c.execute(command)
+        selectedVal = c.fetchall()
+        # list comprehensions -- fetch all tableNames and store in a set
+        tableNames = set([x[1] for x in selectedVal])
+
+        return tableName in tableNames
+
+    def table(self, tableName):
+        '''
+        PRINTS OUT ALL ROWS OF INPUT tableName
+        '''
+        c = self.openDB()
+        command = 'SELECT * FROM "{0}"'.format(tableName)
+        c.execute(command)
+        #print(c.fetchall())
+
+
+    def save(self):
+        '''
+        COMMITS CHANGES TO DATABASE AND CLOSES THE FILE
+        '''
+        self.db.commit()
+        self.db.close()
+    #========================HELPER FXNS=======================
+
+    #======================== DB FXNS =========================
+    def createUsers(self):
+        '''
+        #CREATES TABLE OF users
+        '''
+        self.tableCreator('users', 'user_name text', 'passwords text', 'user_id integer')
+        return True
+
+    def getUsers(self):
+        '''
+        RETURNS A DICTIONARY CONTAINING ALL CURRENT users AND CORRESPONDING PASSWORDS
+        '''
+        c = self.openDB()
+        command = 'SELECT user_name, passwords FROM users'
+        c.execute(command)
+        selectedVal = c.fetchall()
+        return dict(selectedVal)
+
+
+    def registerUser(self, userName, password):
+        '''
+        ADDS user TO DATABASE
+        '''
+        c = self.openDB()
+        command = 'SELECT user_id FROM users WHERE user_id = (SELECT max(user_id) FROM users)'
+        c.execute(command)
+        selectedVal = c.fetchone()
+        max_id = 0
+        if selectedVal != None:
+            max_id = selectedVal[0]
+        else:
+            max_id = 0
+            # userName is already in database -- do not continue to add
+        if self.findUser(userName):
+            return False
+        # userName not in database -- continue to add
+        else:
+            row = (userName, password, max_id + 1)
+            self.insertRow('users', row)
+            return True
+
+    def findUser(self, userName):
+        '''
+        CHECKS IF userName IS UNIQUE
+        '''
+        return userName in self.getUsers()
+
+    def verifyUser(self, userName, password):
+        '''
+        CHECKS IF userName AND password MATCH THOSE FOUND IN DATABASE
+        '''
+        c = self.openDB()
+        command = 'SELECT user_name, passwords FROM users WHERE user_name = "{0}"'.format(userName)
+        c.execute(command)
+        selectedVal = c.fetchone()
+        if selectedVal == None:
+            return False
+        if userName == selectedVal[0] and password == selectedVal[1]:
+            return True
+        return False
+
+
+    #====================== TUESDAY FXNS ======================
+
+
+    #====================== END OF TUESDAY FXNS ======================
+
+
+    #======================== DB FXNS =========================
+#======================== END OF CLASS DB_Manager =========================
+
+# initation process
+'''
+DB_FILE = '../data/tuesday.db'
+initate = DB_Manager(DB_FILE)
+initate.createUsers()
+initate.registerUser('a', 'a')
+initate.save()
+'''
