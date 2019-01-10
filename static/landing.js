8803ff31f862673109a0bab07352fe656a3dcc2f
JoanChirinos <jchirinos@stuy.edu>
2019-01-10 03:14:45 -0500

    added avatar on landing and

--- static/landing.js
+++ static/landing.js
@@ -1,12 +1,18 @@
 window.onload = function () {
+    displayExistingProjects();
+    displayAvatar();
+};
+
+// display avatar
+
+var displayAvatar = function () {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
-            var content = document.getElementById('content');
-            content.innerHTML = this.responseText;
+            document.getElementById('avatar').src = this.responseText;
         }
     }
-    xhttp.open("GET", "/get_snippet?snippet=projectList", true);
+    xhttp.open("GET", "/get_avatar", true);
     xhttp.send();
 };
 
