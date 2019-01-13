58a7c52702e9fb2a7733dd26fe17452b9ce31a85
jwongUlti25 <jwong25@stuy.edu>
2019-01-13 15:56:12 -0500

    project stuff

--- static/project.js
+++ static/project.js
@@ -1,5 +1,6 @@
 window.onload = function () {
     displayAvatar();
+    displayTasks();
 };
 
 var displayAvatar = function () {
@@ -10,5 +11,62 @@ var displayAvatar = function () {
         }
     }
     xhttp.open("GET", "/get_avatar", true);
+    xhttp.send();
+};
+
+
+// dashboard
+var displayDash = function () {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById('contentColumn').innerHTML = this.responseText;
+            // console.log(this.responseText);
+
+        }
+    }
+    xhttp.open("GET", "/get_snippet?snippet=dashboard", true);
+    xhttp.send();
+};
+
+// tasks
+var displayTasks = function () {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById('contentColumn').innerHTML = this.responseText;
+            // console.log(this.responseText);
+
+        }
+    }
+    xhttp.open("GET", "/get_snippet?snippet=tasks", true);
+    xhttp.send();
+};
+
+// team inbox
+var displayTeamInbox = function () {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById('contentColumn').innerHTML = this.responseText;
+            // console.log(this.responseText);
+
+        }
+    }
+    xhttp.open("GET", "/get_snippet?snippet=teamInbox", true);
+    xhttp.send();
+};
+
+// private inbox
+var displayPrivateInbox = function () {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById('contentColumn').innerHTML = this.responseText;
+            // console.log(this.responseText);
+
+        }
+    }
+    xhttp.open("GET", "/get_snippet?snippet=privateInbox", true);
     xhttp.send();
 };
