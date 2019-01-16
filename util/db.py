d4655f08c42f6c8859cf0d3c1e11b122387b6553
th0mazzz <thomaszhao2001@gmail.com>
2019-01-16 15:54:46 -0500

    beenCompleted = 1 when set_status becomes 2

--- util/db.py
+++ util/db.py
@@ -504,6 +504,8 @@ def set_status(status, taskID):
     db = sqlite3.connect(DB_FILE)
     c = db.cursor()
 
+    if status = 2:
+        c.execute('UPDATE tasks SET beenCompleted = 1 WHERE taskID=?', (taskID))
     c.execute('UPDATE tasks SET status = ? WHERE taskID=?', (status, taskID))
 
     db.commit()
